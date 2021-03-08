97dadd3859b491096b42452cbb28f9da2f9ca24e
naresh-sanyasi-au3 <naresspp@gmail.com>
2021-03-08 16:08:48 +0530

    changes

--- server.js
+++ server.js
@@ -51,6 +51,15 @@ app.post("/checkout", async(req, res) => {
 
 });
 
-app.listen(8080, () => {
+const port = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static('build'))
+    const path = require('path')
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
+}
+app.listen(port, () => {
     console.log("server is running");
 });
